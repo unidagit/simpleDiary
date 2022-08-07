0ff78c12ca0ee89f6754760a6e9ae39f6d7c8d82
yunheejo <rhcjrdbsgml@gmail.com>
2022-08-07 23:19:31 +0900

    작성자 기능 추가

--- src/DiaryEditor.jsx
+++ src/DiaryEditor.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 export default function DiaryEditor() {
-  return <div className="DiaryEditor"></div>;
+  const [author, setAuthor] = useState("");
+
+  const onChangeInput = (e) => {
+    setAuthor(e.target.value);
+    console.log(e);
+  };
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <form>
+        <input type="text" value={author} onChange={onChangeInput} />
+      </form>
+    </div>
+  );
 }
