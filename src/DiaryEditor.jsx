c142b7b7ac8f86386256059f48ee7ad99ce4c3ba
yunheejo <rhcjrdbsgml@gmail.com>
2022-08-08 14:21:58 +0900

    감정점수 체크 기능 추가

--- src/DiaryEditor.jsx
+++ src/DiaryEditor.jsx
@@ -50,6 +50,19 @@ export default function DiaryEditor() {
             onChange={handleChangeState}
           />
         </div>
+        <div>
+          <select
+            name="emotion"
+            value={state.emotion}
+            onChange={handleChangeState}
+          >
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+        </div>
       </form>
     </div>
   );
